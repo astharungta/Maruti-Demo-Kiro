84bb07484b88773a67e2160b95b1f7bfd4fbbb28
astharungta <asrungta@deloitte.com>
2026-01-11 16:57:47 +0530

    Automated deployment - 2026-01-11T11:26:27.026Z

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,5 +1,5 @@
 // Auto-generated validation utilities from BRD
-// Generated: 2026-01-11T09:54:46.007Z
+// Generated: 2026-01-11T11:23:20.324Z
 // Source: ExtendedWarranty_Complete_Field_Rules_and_Defaults 1.md
 // Total User Stories: 0
 // Total Field Validations: 115
