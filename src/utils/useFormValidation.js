7532ebf7c5ae035e9e631b192b28906430a1b17b
astharungta <asrungta@deloitte.com>
2026-01-12 16:07:50 +0530

    Automated deployment - 2026-01-12T10:36:37.833Z

--- src/utils/useFormValidation.js
+++ src/utils/useFormValidation.js
@@ -1,5 +1,5 @@
 // Auto-generated validation hook from BRD
-// Generated: 2026-01-12T10:17:14.454Z
+// Generated: 2026-01-12T10:32:13.663Z
 
 import { useState } from 'react';
 import validations from './validations';
