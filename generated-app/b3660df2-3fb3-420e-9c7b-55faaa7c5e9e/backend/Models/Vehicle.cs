3637244a416485d96faf389a2192349b04436d9d
astharungta <asrungta@deloitte.com>
2026-01-27 17:52:41 +0530

    Add backend/Models/Vehicle.cs - Forward Engineering

---
+++ generated-app/b3660df2-3fb3-420e-9c7b-55faaa7c5e9e/backend/Models/Vehicle.cs
@@ -0,0 +1,10 @@
+public class Vehicle
+{
+    public int Id { get; set; }
+    public string Vin { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string Make { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string Color { get; set; } = string.Empty;
+    public int CustomerId { get; set; }
+}
